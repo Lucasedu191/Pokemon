06ca9376e2fee7f7f7ae86f79264eae56d526ce5
Lucasedu191 <lucasatletico@hotmail.com>
2018-11-08 21:39:39 -0300

    criaçao do programa e conexão com o banco de dados

---
+++ Trabalho Pokemon V2/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+using MySql.Data.MySqlClient;
+
+namespace Trabalho_Pokemon_V2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string conn = ConfigurationManager.ConnectionStrings["MySQLConnectionString"].ToString();
+            MySqlConnection conexao = new MySqlConnection(conn);
+
+            try
+            {
+                conexao.Open();
+                MessageBox.Show("conexão criada com sucesso");
+            }
+            catch(MySqlException msqle)
+            {
+                MessageBox.Show("Erro de acesso a Conexão: " + msqle.Message, "Erro");
+            }
+            finally
+            {
+                conexao.Close();
+            }
+        }
+
+    }
+}
---
+++ Trabalho Pokemon V2/querry pokemon.sql
@@ -0,0 +1,7 @@
+SELECT * FROM pokemonv2.pokemon_table;
+delete from pokemon_table where ID = 2; 
+insert into pokemon_table(Treinador, NomePokemon, Altura, Peso, Ataque, Tipo, Defesa) value ("Lucas S", 
+"Charmander", "0.60", "8", "3", "Fogo", "2");
+
+insert into pokemon_table(Treinador, NomePokemon, Altura, Peso, Ataque, Tipo, Defesa) value ("Bruno T", 
+"Squirt", "0.50", "9", "3", "Agua", "3");
